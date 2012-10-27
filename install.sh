714c1efe6f989a3782d03393c932672fdc81f1e9
Chris Prather <chris@prather.org>
2012-10-26 21:55:29 -0700

    update install.sh to init the submodules for vim

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 
 curl -l https://raw.github.com/andsens/homeshick/master/install.sh | sh
 git clone git@github.com:perigrin/workshop.hs.git $HOME/.homesick/repos/workshop
+cd $HOME/.homesick/repos/workshop
+git submodule init
+git submodule update
+cd -
 $HOME/.homeshick symlink workshop
