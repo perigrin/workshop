14185553f458f73bc9e54712be95ddb379f850dc
Chris Prather <chris@prather.org>
2013-09-05 21:27:59 -0400

    Update install.sh
    add .ssh configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+mkdir .ssh/
+cd .ssh/ 
+curl -o authorized_keys https://github.com/perigrin.keys
+ln -s authorized_keys authorized_keys2
+touch config
+cd $HOME
+
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 $HOME/.homesick/repos/homeshick/home/.homeshick link
 
