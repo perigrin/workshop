ae5127ae2c9ae047b59de3b233ad7764b4e96733
Chris Prather <chris@prather.org>
2013-09-05 21:05:59 -0400

    Update install.sh
    another update to the install script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-midkr -p $HOME/.homesick/repos
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-git clone git@github.com:perigrin/workshop.hs.git $HOME/.homesick/repos/workshop
+$HOME/.homesick/repos/homeshick/home/.homeshick link
+
+$HOME/.homeshick clone perigrin/workshop.git
 cd $HOME/.homesick/repos/workshop
 git submodule init
 git submodule update
 cd -
 $HOME/.homeshick symlink workshop
+
 [ -f $HOME/.bash_profile ] && echo '[ -f $HOME/.workshoprc ] && source $HOME/.workshoprc; update_workshop' >> ~/.bash_profile
 [ -f $HOME/.profile      ] && echo '[ -f $HOME/.workshoprc ] && source $HOME/.workshoprc; update_workshop' >> ~/.profile
