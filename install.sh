61c11da1138d43c5bb3c523a62908ba1b51fcf26
Chris Prather <chris@prather.org>
2016-08-29 19:13:42 -0400

    Update install.sh
    adjust for changes in the underlying homeshick install

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ touch config
 cd $HOME
 
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-$HOME/.homesick/repos/homeshick/home/.homeshick link
+source $HOME/.homesick/repos/homeshick/homeshick.sh
 
-$HOME/.homeshick clone https://github.com/perigrin/workshop.git
+homeshick clone https://github.com/perigrin/workshop.git
 cd $HOME/.homesick/repos/workshop
 git submodule init
 git submodule update
 cd -
-$HOME/.homeshick symlink workshop
+homeshick symlink workshop
 
 [ -f $HOME/.bash_profile ] && echo '[ -f $HOME/.workshoprc ] && source $HOME/.workshoprc; update_workshop' >> ~/.bash_profile
 [ -f $HOME/.profile      ] && echo '[ -f $HOME/.workshoprc ] && source $HOME/.workshoprc; update_workshop' >> ~/.profile
